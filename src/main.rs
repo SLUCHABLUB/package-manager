40e30e0c72618707670cf2f32e7fbd9317b20cd6
Nelli Skogman <nelli.skogman@gmail.com>
2026-06-18 17:12:28 +0200

    Add `anyhow`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use anyhow::Context;
+use fs_err::read_to_string;
+use tracing::error;
 use tracing::info;
+
+const MANIFEST_LOCATION: &str = "/etc/package-manager.manifest.toml";
 
 fn main() {
     tracing_subscriber::fmt::init();
 
+    match try_main() {
+        Ok(()) => (),
+        Err(error) => error!("{:#}", error),
+    };
+
     info!("done");
+}
+
+fn try_main() -> anyhow::Result<()> {
+    let _: String = read_to_string(MANIFEST_LOCATION).context("reading package manifest")?;
+
+    Ok(())
 }
